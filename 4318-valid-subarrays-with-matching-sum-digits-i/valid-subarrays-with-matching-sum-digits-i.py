762a5c70cda1419f00dd6530bf917a6d9d977708
Ajisth Sharma <ajisths497@gmail.com>
2026-06-30 22:45:46 +0530

    Time: 3130 ms (96.32%) | Memory: 19.4 MB (90.88%) - LeetSync

--- 4318-valid-subarrays-with-matching-sum-digits-i/valid-subarrays-with-matching-sum-digits-i.py
+++ 4318-valid-subarrays-with-matching-sum-digits-i/valid-subarrays-with-matching-sum-digits-i.py
@@ -1,22 +1,15 @@
 class Solution:
     def countValidSubarrays(self, nums: list[int], x: int) -> int:
-        def isvalid(summ):
-            if summ%10!=x:
-                return False
-
-            temp=summ
-            while temp>=10:
-                temp//=10
-
-            return temp==x
-
-        ans=0
-        for i in range(len(nums)):
-            summ=0
-            for j in range(i,len(nums)):
-                summ+=nums[j]
-
-                if isvalid(summ):
-                    ans+=1
-
+        n = len(nums)
+        ans = 0
+        s = str(x)
+        for i in range(n):
+            summ = 0
+            for j in range(i, n):
+                summ += nums[j]
+                if summ % 10 == x:
+                    if str(summ)[0] == s:
+                        ans += 1
         return ans
+
+            
