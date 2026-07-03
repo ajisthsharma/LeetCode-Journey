2d8cd256314b247ffdcf54400b228ec4c1bd02e0
Ajisth Sharma <ajisths497@gmail.com>
2026-07-03 23:46:04 +0530

    Time: 4 ms (94.62%) | Memory: 19.7 MB (63.45%) - LeetSync

--- 4313-password-strength/password-strength.py
+++ 4313-password-strength/password-strength.py
@@ -1,22 +1,14 @@
 class Solution:
     def passwordStrength(self, password: str) -> int:
-        seen=set()
-        ans=0
+        strength = 0
+        for i in set(password):
+            if i.islower():
+                strength += 1
+            elif i.isupper():
+                strength += 2
+            elif i.isdigit():
+                strength += 3
+            else:
+                strength += 5
 
-        for ch in password:
-            if ch not in seen:
-                if ch.islower():
-                    ans+=1
-                
-                elif ch.isupper():
-                    ans+=2
-
-                elif ch.isdigit():
-                    ans+=3
-
-                else:
-                    ans+=5
-
-            seen.add(ch)
-
-        return ans
+        return strength
