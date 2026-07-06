cd0805c84b07ab8967f0d354cfe4e6802dd63590
Ajisth Sharma <ajisths497@gmail.com>
2026-07-06 23:37:16 +0530

    Time: 7 ms (99.79%) | Memory: 20.5 MB (52.68%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -1,24 +1,19 @@
-
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        freq1 = [0] * 26
-        freq2 = [0] * 26
-
-        for ch in word1:
-            freq1[ord(ch) - ord('a')] += 1
-
-        for ch in word2:
-            freq2[ord(ch) - ord('a')] += 1
-
-        for i in range(26):
-            if (freq1[i] == 0 and freq2[i] != 0) or (freq1[i] != 0 and freq2[i] == 0):
+        len1=len(word1)
+        len2=len(word2)
+        if len1!=len2:
+            return False
+        num_map1=[]
+        num_map2=[]
+        for i in range(97, 123):
+            if chr(i) in word1 and chr(i) in word2:
+                num_map1.append(word1.count(chr(i)))
+                num_map2.append(word2.count(chr(i)))
+            elif (chr(i) in word1 and chr(i) not in word2) or (chr(i) not in word1 and chr(i) in word2):
                 return False
-
-        freq1.sort()
-        freq2.sort()
-
-        for i in range(26):
-            if freq1[i] != freq2[i]:
-                return False
-
-        return True
+        num_map1.sort()
+        num_map2.sort()
+        if num_map2==num_map1:
+            return True
+        return False
