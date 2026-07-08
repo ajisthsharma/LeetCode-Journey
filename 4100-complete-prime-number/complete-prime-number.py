56e4e11061773251edbdf3eed7638a6468fd134f
Ajisth Sharma <ajisths497@gmail.com>
2026-07-09 00:29:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (90.95%) - LeetSync

--- 4100-complete-prime-number/complete-prime-number.py
+++ 4100-complete-prime-number/complete-prime-number.py
@@ -1,36 +1,3 @@
 class Solution:
     def completePrime(self, num: int) -> bool:
-        def is_prime(n):
-            if n <= 1:
-                return False
-            if n <= 3:
-                return True
-            if n % 2 == 0 or n % 3 == 0:
-                return False
-            
-            i = 5
-            
-            while i * i <= n:
-                if n % i == 0 or n % (i + 2) == 0:
-                    return False
-                i += 6
-
-            return True
-
-        if num==2 or num==3 or num==5 or num==7:
-            return True
-        
-        prefix = num
-        while prefix > 0:
-            if not is_prime(prefix):
-                return False
-            prefix //= 10
-        
-        divisor = 10
-        while divisor <= num:
-            suffix = num % divisor
-            if not is_prime(suffix):
-                return False
-            divisor *= 10
-        
-        return True
+        return num in [2, 3, 5, 7, 23, 37, 53, 73, 313, 317, 373, 797, 3137, 3797, 739397]
