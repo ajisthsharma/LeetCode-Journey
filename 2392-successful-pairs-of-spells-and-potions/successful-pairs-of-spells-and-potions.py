74797af0aa33311aa0fdd8825b0f54c59764445c
Ajisth Sharma <ajisths497@gmail.com>
2026-08-11 22:56:18 +0530

    Time: 877 ms (13.08%) | Memory: 45.7 MB (30.72%) - LeetSync

---
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
@@ -0,0 +1,25 @@
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        def solve(potions,strength,success):
+            low,high=0,len(potions)-1
+            idx=-1
+
+            while low<=high:
+                mid=low+(high-low)//2
+
+                if potions[mid]*strength>=success:
+                    high=mid-1
+                    idx=mid
+                else:
+                    low=mid+1
+
+            return idx
+
+        potions.sort()
+        ans=[]
+
+        for spell in spells:
+            idx=solve(potions,spell,success)
+            ans.append(len(potions)-idx if idx!=-1 else 0)
+
+        return ans
