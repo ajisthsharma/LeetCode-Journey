83cb5bd7659ef0ebe875dbed2e4748cc55270209
Ajisth Sharma <ajisths497@gmail.com>
2026-08-25 23:09:15 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (89.46%) - LeetSync

---
+++ 89-gray-code/gray-code.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        int size=1<<n;
+        std:: vector<int> grayCode(size);
+
+        for (int i=0; i<size; i++){
+            grayCode[i]=i^(i>>1);
+        }
+        return grayCode;
+    }
+};
